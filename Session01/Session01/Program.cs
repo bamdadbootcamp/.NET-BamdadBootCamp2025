a8f7f4f9f67e930454663cec51ac5c2fdcdc24fa
taha-th01 <taha.thxar01@gmail.com>
2025-02-15 17:25:12 +0330

    Initial Project

---
+++ Session01/Session01/Program.cs
@@ -0,0 +1,241 @@
+﻿
+
+
+////int num1 = 10;
+
+////Console.WriteLine(num1);
+
+////num1 = 20;
+
+////Console.WriteLine(num1);
+
+//////int num2 = num1;
+
+
+//int num1 = 20;
+//int num2 = 6;
+
+//int sum = num1 + num2;
+//int multiple = num1 * num2;
+//int devide = num1 / num2;
+//int mines = num1 - num2;
+//int division = num1 % num2;
+//int num1PlusOne = num1++;
+//int num1MinesOne = num1--;
+
+//string welcomeText = "Hello Bamdad";
+
+//Console.WriteLine(welcomeText);
+
+
+//string t1 = "10";
+//string t2 = "20";
+
+//string t3 = t1 + t2;
+//Console.WriteLine(t1 + t2);
+
+
+//string text = 10;
+
+//char myChar = 'g';
+
+//// boolean
+//bool myBoolean1 = false;
+//bool myBoolean2 = true;
+
+
+//// GARBAGE COLLECTOR
+
+//bool condition1 = 10 > 3;
+//bool condition2 = 10 >= 3;
+//bool condition3 = 10 < 3;
+//bool condition4 = 10 <= 3;
+//bool condition5 = 10 == 11;
+//bool condition6 = 10 != 11;
+
+//int num1 = 10;
+//int num2 = 20;
+
+//bool cond = num1 == num2;
+
+
+
+//    if ( bool:CONDITION )
+
+
+//bool myCondition = 20 > 11;
+
+////if (myCondition == false)
+//if (!myCondition)
+//{
+//    Console.WriteLine("OK");
+//}
+//else
+//{
+
+//}
+
+
+//string inputText = Console.ReadLine();
+//Console.WriteLine(inputText);
+
+//Console.WriteLine("Welcome");
+//Console.WriteLine("Please enter your age");
+
+//string inputText = Console.ReadLine();
+//int age = int.Parse(inputText);
+
+
+//bool condition = age >= 18;
+
+//if (condition)
+//{
+//    Console.WriteLine("Welcome");
+//}
+//else
+//{
+//    Console.WriteLine("Sorry, Bye");
+//}
+
+
+//int averageScore = 19;
+//int mathScore = 17;
+
+//bool condition1 = averageScore >= 18;
+//bool condition2 = mathScore >= 16;
+//bool condition3 = true;  
+//bool condition4 = true;
+
+
+//bool test = condition1 && condition2 && condition3 && condition4;
+
+
+//bool ANDcond = condition1 && condition2;
+//bool ORcond = condition1 || condition2;
+
+//int num1;
+//string mytext;
+//Console.WriteLine(num1);
+
+//bool? mycondition;
+
+//if (mycondition)
+//{
+
+//}
+
+// EXCEPTIONS
+// 1- Compile Time Exception
+// 2- Run Time Exception
+
+//Console.WriteLine("Please select game level");
+//Console.WriteLine("[1]: Easy");
+//Console.WriteLine("[2]: Medium");
+//Console.WriteLine("[3]: Hard");
+
+////string inputText = Console.ReadLine();
+////int level = int.Parse(inputText);
+
+//int level = int.Parse(Console.ReadLine());
+
+//if (level == 1)
+//{
+//    Console.WriteLine("1");
+//}
+//else if (level == 2)
+//{
+//    Console.WriteLine("2");
+//}
+//else if (level == 3)
+//{
+//    Console.WriteLine("3");
+//}
+//else
+//{
+//    Console.WriteLine("Wrong Number");
+//}
+
+//decimal myvar = ;
+
+//decimal price = 100000.00m / 3;
+
+
+//decimal price2 = 10.2m;
+
+//decimal salary = 100_000_000_000;
+
+//Console.WriteLine();
+
+
+//DateTime birt = DateTime.Parse("1996-05-18 06:18:32");
+
+//DateTime now = DateTime.Now;
+
+//TimeSpan duration = now - navidBirthday;
+
+//DateOnly dateOnly = DateOnly.FromDateTime(now);
+//TimeOnly timeOnly = TimeOnly.FromDateTime(now);
+
+//TimeSpan 
+//DateTime now1 = DateTime.Now;
+//DateTime now2 = DateTime.Now;
+
+//                DateTime DateTime
+//                TimeOnly TimeOnly
+//                DateOnly DateOnly
+//                DateTime TimeOnly => ERROR
+//TimeSpan duration = now1 - now2;
+
+//DateTime minValue = DateTime.MinValue;
+//DateTime maxValue = DateTime.MaxValue;
+
+//DateTime date = DateTime.Now;
+
+////date = date.AddDays(100); 
+
+
+
+//Console.WriteLine(date.DayOfWeek);
+//Console.WriteLine(date.Minute);
+//Console.WriteLine(date.DayOfYear);
+//Console.WriteLine(date.Date);
+
+//string num = "10";
+//var num1 = 100000000000;
+
+//Console.WriteLine(num1.GetType());
+
+string text = "A";
+
+switch (text)
+{
+    case "A":
+        Console.WriteLine("");
+        break;
+    case "B":
+        Console.WriteLine("B");
+        break;
+    case "C":
+        Console.WriteLine("C");
+        break;
+    default:
+        Console.WriteLine("Default");
+        break;
+}
+// ---------------------------------------------------
+if (text == "A")
+{
+    Console.WriteLine("A");
+}
+else if (text == "B")
+{
+    Console.WriteLine("B");
+}
+else if (text == "C")
+{
+    Console.WriteLine("C");
+}
+else
+{
+    Console.WriteLine("Default");
+}
---
+++ Session02/Session02/Program.cs
@@ -0,0 +1,151 @@
+﻿
+
+
+// متغیر های عددی
+// byte, short, int, long 
+//      ushort,uint,ulong     منفی نمیپذیرند
+
+
+//int num1 = 20;
+
+//Console.WriteLine(num1);
+
+//num1 = num1 + 10; ;
+
+//Console.WriteLine(num1);
+
+// رشته و کاراکتر
+// char      string
+
+//string firstName = "Sajad";
+//string lastName = "Rezaei";
+//byte age = 21;
+
+//// Sajad Rezaei has 21 years old
+
+//string result = firstName + " " + lastName + " has " + age.ToString() + " years old";
+////Console.WriteLine(result);
+
+
+
+//// <ANY DATA TYPE>   ==TO==> STRING           :    <DATA TYPE>.ToString()
+////      STRING       ==TO==> <ANY DATA TYPE>  :   <ANY DATA TYPE>.Parse(<STRING VALUE>)
+
+//string newResult = "{firstName} {lastName} has {age} years old";
+//Console.WriteLine(newResult);
+
+
+//bool cond1 = false;
+//bool cond2 = true;
+//bool cond3 =          234 >= 234      &&      10 > 6;
+////   true              true          &&       true
+
+
+
+
+//Console.WriteLine("Please enter your age");
+//string inputText = Console.ReadLine();
+
+//int age = int.Parse(Console.ReadLine());
+
+// IF TERNARY OPERATOR
+// <DATA TYPE> varname = <CONDITION 1> ? <VALUE 1> :
+//                       <CONDITION 2> ? <VALUE 2> :
+//                       <CONDITION 3> ? <VALUE 3> :
+//                       <CONDITION 4> ? <VALUE 4> :
+//                       <CONDITION 5> ? <VALUE 5> :
+//                       <CONDITION 6> ? <VALUE 6> :
+//                       <CONDITION N> ? <VALUE N> :
+//                       <ELSE VALUE>
+
+
+//string showText = "";
+//if (age >= 18)
+//{
+//    showText = "Welcome";
+//}
+//else
+//{
+//    showText = "Sorry";
+//}
+
+//string showText = age >= 18 ? "Welcome" : "Sorry";
+//string showText = age >= 18 ? "Welcome" :
+//                  age <= 30 && age > 25 ? "Hi" :
+//                    "Sorry";
+
+//Console.WriteLine(showText);
+
+//int? num1 = null;
+
+
+
+
+
+// حلقه ها loops
+// حلقه ها برای ما یک عملیات تکرار شونده انجام میدهد
+// حلقه ها حتماااااا و حتما باید شرط اتمام داشته باشند
+// وگرنه داخل لوپ بی نهایت میشوند Inifinity Loop
+
+
+// loop : for
+
+// این حلقه به شما یک متغیر میدهد که در هر بار اجرای آن
+// مقدار آن تغییر میکند
+// به صورت پیشفرض مقدار آن i میباشد
+// برای این حلقه نوع 
+// Increament 
+// آن باید مشخص شود
+// شرط اتمام نیز باید مشخص شود
+
+
+//      loop variable    condition      loop increament/decreament
+//  for (int i = 0    ;    i  <  10   ;   i++   )   
+//  {
+//          //    BODY
+//  }
+
+//for (int i = 1; i <= 10;i++)
+//{
+//    Console.WriteLine($"Number is {i}");
+//}
+//for (int i = 10; i >= 1; i--)
+//{
+//    string test = "test";
+//    Console.WriteLine($"Number is {i} {test}");
+//    if (10 > 20)
+//    {
+//        int num1 = 10;
+        
+//    }
+    
+//}
+//Console.WriteLine($"Number is {i}");
+
+// Version Control
+// git : GitHub - GitLab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
