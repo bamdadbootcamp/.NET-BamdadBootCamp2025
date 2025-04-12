544df9d64dedb2a342a2ed893213f426c827709f
taha-th01 <taha.thxar01@gmail.com>
2025-04-12 18:12:51 +0330

    session 10

---
+++ Session10/BookStore/BookStore.BL/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.BL.Entities.Core;
+
+namespace BookStore.BL.Entities;
+
+public class Book : BaseEntity
+{ 
+    public Book()
+    {
+    }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int PageCount { get; set; }
+    public string ISBN { get; set; }
+    public DateOnly PublishDate { get; set; }
+
+    public int BookCategoryId { get; set; }
+    public int PublisherId { get; set; }
+}
---
+++ Session10/BookStore/BookStore.BL/Entities/Core/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.BL.Entities.Core;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime LastModifiedDate { get; set; }
+    public bool IsDeleted { get; set; }
+}
