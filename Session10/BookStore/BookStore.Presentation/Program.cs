94ebf9ef7d4f6717931f91c5e9e6a25b46a9d2cb
bamdad.bootcamp.a@gmail.com <bamdad.bootcamp.a@gmail.com>
2025-04-26 15:26:43 +0330

    up

--- Session10/BookStore/BookStore.Presentation/Program.cs
+++ Session10/BookStore/BookStore.Presentation/Program.cs
@@ -11,6 +11,7 @@ namespace BookStore.Presentation
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
             Application.Run(new Form1());
         }
     }
